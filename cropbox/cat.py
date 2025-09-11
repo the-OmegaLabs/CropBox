2a4889774e9eb2c7c513a8b7a3ff41e9bf9de0c1
bzym2pc <kamisatoayakamo@yandex.com>
2025-09-11 19:50:57 +0800

    操逼

--- cropbox/cat.py
+++ cropbox/cat.py
@@ -1,113 +1,148 @@
 # OmegaOS CropBox - cat
+# Author: Stevesuk0 (stevesukawa@outlook.com)
+# Modification: bzym2 (yakamoawa@outlook.com)
+# Modification: Google Gemini 2.5 Pro (aistudio.google.com)
+#
+# Description:
+#     OmegaOS CropBox version of the 'cat' command.
+#     Maybe compatible with POSIX 'cat' behavior, with minor adjustments.
+#
 # Usage:
 #     cat [OPTIONS]... [FILE]...
-#     Print FILEs to stdout.
 #
-# Author: 
-#     Stevesuk0 (stevesukawa@outlook.com)
+# Function:
+#     Reads each FILE in sequence and writes its contents to standard output.
+#     If no FILE is provided or FILE is '-', reads from standard input.
 #
 # Options:
-#     -n    Number all output lines.
-#     -b    Number only non-empty output lines.
-#     -s    Squeeze multiple consecutive blank lines into one.
-#     -e    Display '$' at the end of each line.
-#     -t    Display TAB characters as '^I'.
+#     -    Argument head.
+#     n    Number all output lines.
+#     b    Number only non-empty output lines.
+#     s    Squeeze multiple consecutive blank lines into one.
+#     E    Display '$' at the end of each line.
+#     T    Display TAB characters as '^I'.
+#
+# Examples:
+#     cat file.txt
+#         Print the contents of file.txt to standard output.
+#
+#     cat
+#         Read from standard input until EOF (Ctrl+D).
 
 import os
 import sys
 import string
 
-class Main:
+from cropbox.commandBase import CommandBase
+
+
+class Cat(CommandBase):
     def __init__(self, args):
+        super().__init__(args)
         self.count_line = False
         self.count_line_skip_empty = False
         self.end_sign = False
         self.show_tabs = False
         self.show_nonprint = False
-        self.flush_immediately = False
 
-        if len(args) == 0:
-            self.cat_stdin()
+        self.options = []
+        self.files = []
+
+    def parse_args(self):
+        for arg in self.args:
+            if arg.startswith('-') and arg != '-':
+                # 这样可以支持像 -nE 这样的组合 很爽
+                for char in arg[1:]:
+                    if char == 'n':
+                        self.count_line = True
+                    elif char == 'b':
+                        self.count_line = True
+                        self.count_line_skip_empty = True
+                    elif char == 'E':
+                        self.end_sign = True
+                    elif char == 'T':
+                        self.show_tabs = True
+                    elif char == 'v':
+                        self.show_nonprint = True
+                    elif char == 'A':
+                        self.end_sign = True
+                        self.show_tabs = True
+                        self.show_nonprint = True
+                    elif char == 'u':
+                        # POSIX -u 选项是禁用输出缓冲，Python的 print 默认就是行缓冲
+                        pass
+                    else:
+                        self.error(f"invalid option -- '{char}'")
+            else:
+                # 那这就是一个文件名或 stdin ('-')
+                self.files.append(arg)
+
+    def run(self):
+        self.parse_args()
+
+        if not self.files:
+            # 没有文件就直接从stdin里读
+            self.process_stdin()
         else:
-            for i in args:
-                if i == '-':
-                    self.cat_stdin()
-                elif i == '-n':
-                    self.count_line = True
-                elif i == '-b':
-                    self.count_line = True
-                    self.count_line_skip_empty = True
-                elif i == '-e':
-                    self.end_sign = True
-                elif i == '-t':
-                    self.show_tabs = True
-                elif i == '-v':
-                    self.show_nonprint = True
-                elif i == '-A':  # show-all
-                    self.end_sign = True
-                    self.show_tabs = True
-                    self.show_nonprint = True
-                elif i == '-u':
-                    self.flush_immediately = True
-                elif os.path.exists(i):
-                    self.cat_file(i)
+            for file_path in self.files:
+                if file_path == '-':
+                    self.process_stdin()
+                elif os.path.exists(file_path):
+                    if os.path.isdir(file_path):
+                        self.error(f"{file_path}: Is a directory")
+                    else:
+                        self.process_file(file_path)
                 else:
-                    print(f"cat: {i}: No such file or directory", file=sys.stderr)
-                    sys.exit(1)
+                    self.error(f"{file_path}: No such file or directory")
 
     def make_visible(self, text):
         res = ''
         for c in text:
             if c == '\t' and self.show_tabs:
                 res += '^I'
-            elif c not in string.printable and self.show_nonprint:
-                res += '^' + chr(ord(c) ^ 0x40)
+            elif (ord(c) < 32 or ord(c) > 126) and c not in ('\t', '\n') and self.show_nonprint:
+                # M-x (Meta-x) for characters > 127
+                if ord(c) > 127:
+                    res += 'M-^' + chr((ord(c) - 128) ^ 0x40)
+                # ^x for control characters
+                else:
+                    res += '^' + chr(ord(c) ^ 0x40)
             else:
                 res += c
         return res
 
-    def cat_file(self, path):
+    def process_stream(self, stream):
+        cnt = 0
+        for line in stream:
+            line_content = line.rstrip('\n')
+
+            # 处理 -b 选项（只计数非空行）
+            if self.count_line:
+                is_empty = not line_content.strip()
+                if self.count_line_skip_empty and is_empty:
+                    sys.stdout.write('\n')
+                    continue
+                cnt += 1
+                sys.stdout.write(f"{cnt:6}\t")
+
+            line_content = self.make_visible(line_content)
+            sys.stdout.write(line_content)
+
+            if self.end_sign:
+                sys.stdout.write('$')
+
+            sys.stdout.write('\n')
+        sys.stdout.flush()
+
+    def process_file(self, path):
         with open(path, 'r', errors='replace') as f:
-            cnt = 0
-            for i in f:
-                line = i.rstrip('\n')
-                if self.count_line:
-                    if self.count_line_skip_empty and not line.strip():
-                        sys.stdout.write('\n')
-                        continue
-                    cnt += 1
-                    sys.stdout.write(f'{cnt}  ')
-                line = self.make_visible(line)
-                sys.stdout.write(line)
-                if self.end_sign:
-                    sys.stdout.write('$')
-                sys.stdout.write('\n')
-                if self.flush_immediately:
-                    sys.stdout.flush()
+            self.process_stream(f)
 
-    def cat_stdin(self):
-        while True:
-            content = input()
-
-            raw_content = str(content.encode())[2:-1]
-
-            if raw_content == '\\x04':
-                break
-            else:
-                raw_content = self.make_visible(raw_content)
-                sys.stdout.write(raw_content)
-                if self.end_sign:
-                    sys.stdout.write('$')
-                sys.stdout.write('\n')
-                if self.flush_immediately:
-                    sys.stdout.flush()
-            
+    def process_stdin(self):
+        self.process_stream(sys.stdin)
 
 
-try:
-    args = sys.argv
-    args.pop(0) # clear module path
-
-    Main(args)
-except KeyboardInterrupt:
-    pass
+# 先这么干 做假入口
+# 因为主文件还没写 写了再弄
+if __name__ == "__main__":
+    Cat.main()
